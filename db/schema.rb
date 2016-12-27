422eef7581d0d22488c86affec45c30dd8156042
Holy <hotinly@163.com>
2016-12-28 00:20:51 +0800

    New environment(not use Bitnami)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013233036) do
+ActiveRecord::Schema.define(version: 20161013051247) do
 
   create_table "bknotes", force: :cascade do |t|
     t.integer  "book_id",    limit: 4
